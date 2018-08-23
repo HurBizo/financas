2e999ffee2fecd272b46f7a6139b670f905bdfa4
Smile <31359192+HurBizo@users.noreply.github.com>
2018-08-23 09:09:12 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,33 +1,46 @@
-def selecao(s)
-def menu():
-  s = input('''Menu:
-        1 - Dadois financeiros
-        2 - Sair ''')
-def soma(cred,alu,sla,luz):
-  r = cred+alu+sla+luz
-  return print("Você deve por mês: R$",r)
-  
 
-
-print("Bem vindo! Complete com os seus dados financeiros.
-      \n")
-
-
-cred = int(input("Cartão de crédito: R$"))
-alu = int(input("Aluguel: R$"))
-sla = int(input("Agua: R$"))
-luz = int(input("Luz: R$"))
-soma(cred,alu,sla,luz)
-
-input("---------------PRESIONE ENTER PARA CONTINUAR------------------")
-
-
-
-
-
-
-
-
-
-
-
+import os
+class Fina:
+    def limpar():
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+    def novoDes():
+        Fina.limpar()
+        nome = input("Digite o nome da despesa: ")
+        i = int(input("Valor: R$"))
+        print(nome, "-", i)
+        input("\nAdicionado com sucesso! precione ENTER para continuar\n\n")
+        Fina.limpar()
+        Fina.menu()
+        
+    def ajuste():
+        print("Bem vindo! Digite suas informaçoes.")
+    
+        card = int(input("Cartão: "))
+        luz = int(input("Luz: "))
+        agua = int(input("Agua: "))
+        alugue = int(input("Aluguel: "))
+    
+        input("Suas despesas são de R$",int(r) ," !")
+    
+        
+    def menu():
+        Fina.limpar()
+        s = int(input('''
+Escolha una opção de sua prederencia:
+1 - Ajuste de dados.
+2 - Adicionar despesas
+3 - 
+4 - 
+'''))
+        if(s == 1):
+        	    Fina.ajuste()
+        elif(s == 3):
+        	    print("ok")
+        elif(s == 2):
+            Fina.novoDes()
+            
+        else:
+            print("fim!")
+        	
+Fina.menu()
