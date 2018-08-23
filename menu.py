9ebbb4b9a56b1b35830f30b427a7b366bd8041ee
Smile <31359192+HurBizo@users.noreply.github.com>
2018-08-23 16:09:59 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,29 +1,52 @@
 
 import os
-cliente = []
+
+Nome = ["Cartão","Luz","Agua","Aluguel"]
+Valor = [0]
+
 class Fina:
     def limpar():
         os.system('cls' if os.name == 'nt' else 'clear')
         
     def novoDes():
         Fina.limpar()
-        cliente = [input("Digite o nome da despesa: ")]
-        cliente = [(input("Valor: R$"))]
+        Valor.pop()
+        Nome.append(input("Digite o nome da despesa: "))
+        Valor.append(input("Valor: R$"))
         print(nome, "-", i)
         input("\nAdicionado com sucesso! precione ENTER para continuar\n\n")
         Fina.limpar()
         Fina.menu()
+    def count(a):
+        return a + 1
+    
+    def soma():
+        Valor = []
         
     def ajuste():
+        Fina.limpar()
         print("Bem vindo! Digite suas informaçoes.")
-    
-        card = int(input("Cartão: "))
-        luz = int(input("Luz: "))
-        agua = int(input("Agua: "))
-        alugue = int(input("Aluguel: "))
-    
-        input("Suas despesas são de R$",int(r) ," !")
-    
+        
+        Valor.append(int(input("Cartão: ")))
+        Valor.append(int(input("Luz: ")))
+        Valor.append(int(input("Agua: ")))
+        Valor.append(int(input("Aluguel: ")))
+        
+        r = sum(Valor)
+        print("Suas despesas são de R$",r )
+        input()
+        
+        Fina.menu()
+        
+    def resumo():
+        Fina.limpar()
+        if Valor == 0:
+          print("Você ainda não preencheu seu quadro financeiro!")
+          input()
+          Fina.ajuste()
+        print('''
+ Seu resumo geral ficou
+''',sum(Valor))
         
     def menu():
         Fina.limpar()
@@ -31,17 +54,21 @@ class Fina:
 Escolha una opção de sua prederencia:
 1 - Ajuste de dados.
 2 - Adicionar despesas
-3 - 
-4 - 
+3 - Resumo geral
+4 - Adicionar lucro
 '''))
+        print(Valor)
+        input()
+        
         if(s == 1):
-        	    Fina.ajuste()
+          Fina.ajuste()
         elif(s == 3):
-        	    print("ok")
+    	    Fina.resumo()
         elif(s == 2):
-            Fina.novoDes()
-            
+          Fina.novoDes()
+                
         else:
-            print("fim!")
+          print("fim!")
+       
         	
 Fina.menu()
