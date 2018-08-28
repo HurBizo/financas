a42b1a5ff6f74a85154e784a414b2af3a564111f
Smile <31359192+HurBizo@users.noreply.github.com>
2018-08-28 10:08:41 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -7,31 +7,32 @@ Valor = [0]
 class Fina:
     def limpar():
         os.system('cls' if os.name == 'nt' else 'clear')
+    def db(valor,name):
+        db = dbm.open('contatos.db', 'c')
+        db[name] = valor
+        print("Despesa: ",name,"Valor:",db[name].decode(encoding="utf-8"))
+        db.close() 
         
     def novoDes():
         Fina.limpar()
-        db = dbm.open('contatos.db', 'c')
+        
         v = input("Valor: ")
         s = input("Nome: ")
-        db[s] = v 
+        db(v,s)
         print(db[s])
         input("\nAdicionado com sucesso! precione ENTER para continuar\n\n")
         Fina.limpar()
         Fina.menu()
     def count(a):
-        return a + 1
-    
-    def soma():
-        Valor = []
-        
+        return a + 1     
     def ajuste():
         Fina.limpar()
         print("Bem vindo! Digite suas informaçoes.")
-        
-        Valor.append(int(input("Cartão: ")))
-        Valor.append(int(input("Luz: ")))
-        Valor.append(int(input("Agua: ")))
-        Valor.append(int(input("Aluguel: ")))
+        db = dbm.open('contatos.db', 'c')
+        db['Cartão'] = input("Cartão: ")
+        db['Luz'] = input("Luz: ")
+        db['Agua'] = input("Agua: ")
+        db['Aluguel'] = input("Aluguel: ")
         
         r = sum(Valor)
         print("Suas despesas são de R$",r )
